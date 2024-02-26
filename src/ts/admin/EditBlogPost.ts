593036a712d830abea96d617fa64398ba607b843
Oliver Booth <git@olivr.me>
2024-02-26 17:43:58 +0000

    feat: add syntax highlighting to post editor

--- src/ts/admin/EditBlogPost.ts
+++ src/ts/admin/EditBlogPost.ts
@@ -1,5 +1,8 @@
 import BlogPost from "../app/BlogPost";
 import API from "../app/API";
+import UI from "../app/UI";
+
+declare const Prism: any;
 
 (() => {
     getCurrentBlogPost().then(post => {
@@ -7,9 +10,12 @@ import API from "../app/API";
             return;
         }
 
-        const saveButton = document.getElementById("save-button");
-        const preview = document.getElementById("article-preview");
+        const saveButton = document.getElementById("save-button") as HTMLButtonElement;
+        const preview = document.getElementById("article-preview") as HTMLAnchorElement;
         const content = document.getElementById("content") as HTMLTextAreaElement;
+        const title = document.getElementById("post-title") as HTMLInputElement;
+        const highlighting = document.getElementById("highlighting");
+        const highlightingContent = document.getElementById("highlighting-content");
 
         saveButton.addEventListener("click", async (e: MouseEvent) => {
             await savePost();
@@ -20,6 +26,22 @@ import API from "../app/API";
                 e.preventDefault();
                 await savePost();
                 preview.innerHTML = post.content;
+                UI.updateUI(preview);
+                // Prism.highlightAllUnder(preview);
+            }
+        });
+
+        content.addEventListener("keydown", async (e: KeyboardEvent) => {
+            if (e.key === "Tab") {
+                e.preventDefault();
+
+                const start = content.selectionStart;
+                const end = content.selectionEnd;
+                const text = content.value;
+                content.value = `${text.slice(0, start)}    ${text.slice(start, end)}`;
+                updateEditView();
+                content.selectionStart = start + 4;
+                content.selectionEnd = end ? end + 4 : start + 4;
             }
         });
 
@@ -42,6 +64,34 @@ import API from "../app/API";
                 saveButton.classList.remove("btn-success");
                 saveButton.innerHTML = '<i class="fa-solid fa-floppy-disk fa-fw"></i> Save <span class="text-muted">(Ctrl+S)</span>';
             }, 2000);
+        }
+
+        updateEditView();
+        content.addEventListener("input", () => updateEditView());
+        content.addEventListener("scroll", () => syncEditorScroll());
+        function updateEditView() {
+            highlightingContent.innerHTML = Prism.highlight(content.value, Prism.languages.markdown);
+            document.querySelectorAll("#highlighting-content span.token.code").forEach(el => {
+                const languageSpan = el.querySelector(".code-language") as HTMLSpanElement;
+                if (!languageSpan) {
+                    return;
+                }
+
+                const language = languageSpan.innerText;
+                const span = el.querySelector(".code-block");
+                if (!span) {
+                    return;
+                }
+
+                span.outerHTML = `<code class="${span.className} language-${language}" style="padding:0;">${span.innerHTML}</code>`;
+                Prism.highlightAllUnder(highlightingContent);
+            });
+            syncEditorScroll();
+        }
+        
+        function syncEditorScroll() {
+            highlighting.scrollTop = content.scrollTop;
+            highlighting.scrollLeft = content.scrollLeft;
         }
     });
 
